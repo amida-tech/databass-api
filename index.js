b604d1279189e8a85d4d9b2234269948de9bc19f
Sean Bolak <seanbolak@gmail.com>
2016-08-22 10:34:28 -0400

    removed todo in index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app        = express();
 const jsonParser = bodyParser.json();
 
 /* Routes */
-//TODO: Add passport token auth middleware to route once tokenStrategy has something in it.
 app.get('/api/user', jsonParser, User.authenticate, User.getCurrentUser);
 
 app.listen(8080, function() {
