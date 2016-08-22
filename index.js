73228b72f67bdce2eb32e728872f9374a1fdc69e
Sean Bolak <seanbolak@gmail.com>
2016-08-22 10:19:12 -0400

    added token authentication middleware

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const User       = require('./user');
 const app        = express();
 const jsonParser = bodyParser.json();
 
-
 /* Routes */
 //TODO: Add passport token auth middleware to route once tokenStrategy has something in it.
-app.get('/user', jsonParser, User.getCurrentUser);
+app.get('/user', jsonParser, User.authenticate, User.getCurrentUser);
 
 app.listen(8080, function() {
   console.log('Server Started at port 8080');
--- user/index.js
+++ user/index.js
@@ -1,14 +1,20 @@
 const Token = require('./tokenModel');
 
 module.exports = {
-  getCurrentUser: function(req, res) {
+  authenticate: function(req, res, next) {
     const requestToken = req.get('token');
     if (requestToken) {
-      Token.findOne({where: {body: req.get('token')}}).then((token) => {
-        res.send(JSON.stringify({userId: token.userId, realm: token.realm}));
+      Token.findOne({where: {body: req.get('token')}, attributes: ['userId', 'realm']}).then((token) => {
+        req.user = token.userId;
+        req.realm = token.realm;
+        next();
       });
     } else {
       res.sendStatus(401);
     }
+  },
+  getCurrentUser: function(req, res) {
+    const currentUser = { userId: req.user, realm: req.realm};
+    res.send(JSON.stringify(currentUser));
   }
 };
