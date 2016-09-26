41dc434784957cc6a1ccbdff37e7095dd0b12445
Sean Bolak <seanbolak@gmail.com>
2016-09-26 10:44:38 -0400

    fixed some syntax with method calls wihtin activity controller

--- activity/controller.js
+++ activity/controller.js
@@ -2,22 +2,25 @@ const Activity = require('./model');
 
 const ActivityController = {
   showActivities: (req, res) => {
-    Activity.findAll().then(activities => {
-      return res.json(activities);
-    });
+    Activity
+      .findAll()
+      .then(activities => {
+        return res.json(activities);
+      });
   },
   findActivityWithPolicyName: (req, res) => {
     Activity
-    .findAll(
-      {
-        where:
+      .findAll(
         {
-          PolicyNumber: req.params.policyNumber
+          where:
+          {
+            PolicyNumber: req.params.policyNumber
+          }
         }
-      }
-    ).then(activities => {
-      return res.json(activities)
-    });
+      )
+      .then(activities => {
+        return res.json(activities)
+      });
   }
 };
 
