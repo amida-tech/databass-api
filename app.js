1ae3bd36377249155ff027753badd4924d465921
Sean Bolak <seanbolak@gmail.com>
2016-09-29 13:43:52 -0400

    changed config to use .env settings

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express    = require('express');
 const cors       = require('cors');
